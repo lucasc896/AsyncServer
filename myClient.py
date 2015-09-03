cce235167869c2daaafc836d0218f2e5b21a266a
Chris Lucas <chris.lucas@bristol.ac.uk>
2015-09-03 19:53:44 +0100

    add in commented command line options (useful later when running multiple instances); add in stock phrase loop

--- myClient.py
+++ myClient.py
@@ -1,15 +1,31 @@
 #!/usr/bin/env python
 import socket as sk
-from sys import argv
+# from sys import argv
 
-if len(argv) > 1:
-    msg = argv[1]
-else:
-    msg = "Hey Mum!"
+# if len(argv) > 1:
+#     msg = argv[1]
+# else:
+#     msg = "Hey Mum!"
+
+BUFSIZE = 4096
 
 sock = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
 serverAddress = ('localhost', 6666)
 print "> Trying to connect to server on %s (port: %s)" % serverAddress
 sock.connect(serverAddress)
 
-sock.sendall(msg)
+# while True:
+#     msg = raw_input("Input:")
+for msg in ["hey", "how are you?", "i'm ok.\n"]:
+    if msg:
+        sock.sendall(msg)
+    else:
+        break
+
+while True:
+    msg = sock.recv(BUFSIZE)
+    
+    if not msg: break
+
+    print "Received: %s" % msg
+
