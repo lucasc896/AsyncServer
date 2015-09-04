1a5997c2322a4b63450b7134bcc37a9bb5748e62
Chris Lucas <chris.lucas@bristol.ac.uk>
2015-09-04 10:10:06 +0100

    move receiving outside of message loop, to suit this brief

--- myClient.py
+++ myClient.py
@@ -9,7 +9,7 @@ else:
 
 BUFSIZE = 4096
 
-socks = [sk.socket(sk.AF_INET, sk.SOCK_STREAM),sk.socket(sk.AF_INET, sk.SOCK_STREAM)]
+socks = [sk.socket(sk.AF_INET, sk.SOCK_STREAM),sk.socket(sk.AF_INET, sk.SOCK_STREAM)][:1]
 print socks
 serverAddress = ('localhost', 6666)
 for sock in socks:
@@ -17,14 +17,14 @@ for sock in socks:
     sock.connect(serverAddress)
     print "> Connected to", serverAddress
 
-for msg in [mesg, "i'm ok.\n"]:
+for msg in [mesg, mesg+"!!", "i'm ok.\n"]:
         
     for sock in socks:
         print "Sending", msg
         sock.sendall(msg)
     
-    for sock in socks:
-        msg = sock.recv(BUFSIZE)
-        print "Received: %s" % msg
-        if not msg:
-            sock.close()
+for sock in socks:
+    msg = sock.recv(BUFSIZE)
+    print "Received: %s" % msg
+    if not msg:
+        sock.close()
