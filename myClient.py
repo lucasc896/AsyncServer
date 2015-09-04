17b97a257986922489f7653c826c0f1d90bccf23
Chris Lucas <chris.lucas@bristol.ac.uk>
2015-09-04 12:13:50 +0100

    add contact details

--- myClient.py
+++ myClient.py
@@ -3,6 +3,18 @@ import socket as sk
 from sys import argv
 from time import sleep
 
+#---------------------------------------------------------------------#
+
+#---------------------------------------------#
+# Written by Chris Lucas (4th September 2015) #
+# lucasc896@gmail.com                         #
+#                                             #
+# Covering letter:  goo.gl/RlBRGF             #
+# CV:               goo.gl/rmmKht             #
+#---------------------------------------------#
+
+#---------------------------------------------------------------------#
+
 if len(argv) > 1:
     mesg = argv[1]
 else:
@@ -11,7 +23,7 @@ else:
 BUFSIZE = 4096
 
 # how many different sockets/clients
-socks = [sk.socket(sk.AF_INET, sk.SOCK_STREAM) for i in range(3)]
+socks = [sk.socket(sk.AF_INET, sk.SOCK_STREAM) for i in range(1)]
 
 serverAddress = ('localhost', 6666)
 
@@ -26,7 +38,7 @@ for i in range(1): #how many rounds of messages
         for sock in socks:
             print "Sending", mesg+msg
             sock.sendall(mesg+msg)
-            sleep(1.8)
+            sleep(1.)
         
     for sock in socks:
         msg = sock.recv(BUFSIZE)
