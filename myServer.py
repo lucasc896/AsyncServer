6e1675790d27c0c5009237ee5611806605081a1e
Chris Lucas <chris.lucas@bristol.ac.uk>
2015-09-04 10:51:41 +0100

    add message buffer container; update splash screen; add timeout to select; end of line catching; add logging

--- myServer.py
+++ myServer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 import socket as sk
 import select as sl
-import logging
-# import errno
-# from time import sleep
+import logging as lg
+from optparse import OptionParser
 
 #---------------------------------------------------------------------#
 # TO-DO
@@ -12,10 +11,39 @@ import logging
 
 #---------------------------------------------------------------------#
 
+class MessageBuf(object):
+    """short container for message to echo"""
+    def __init__(self):
+        self._msg = []
+        self._echo = False
+
+    def Add(self, string = ""):
+        """populate the message"""
+        # note: strip leading/trailing whitespace for neatness
+        self._msg.append(string.strip())
+
+    def EchoReady(self, choice = None):
+        """dual-use function dealing with echo readiness"""
+        if choice:
+            self._echo = bool(choice) # DODGY?
+        else:
+            return self._echo
+
+    def Get(self):
+        """return the message"""
+        return " ".join(self._msg)
+
+    def Reset(self):
+        """re-initialise"""
+        self._msg = []
+        self._echo = False
+
+#---------------------------------------------------------------------#
+
 class AceyncServer(object):
     """an asynchronous echo server"""
     def __init__(self, debug = False):
-        print "\nCreating AceyncServer instance...\n"
+        self.splash()
         self._debug = debug
         self._serverAddress = ('localhost', 6666)
         self._BUFSIZE = 4096
@@ -23,14 +51,21 @@ class AceyncServer(object):
         self._socks = dict.fromkeys(['in', 'out', 'read', 'write', 'bad'], [])
         self._msgBuffers = {}
 
+    def splash(self):
+        w = 29
+        print "\n", "="*w
+        print "-      AceyncServer v0      -"
+        print "-      by Chris Lucas       -"
+        print "="*w, "\n"
+
     def startListening(self):
         """main workhorse function"""
 
         # create main listener socket
         self._serverSock = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
 
-        if self._debug:
-            print "> Starting server on %s (port: %s)" % self._serverAddress
+        lg.debug("> Starting server on %s (port: %s)" % self._serverAddress)
+
         self._serverSock.bind(self._serverAddress)
 
         # make it non-blocking!
@@ -46,43 +81,45 @@ class AceyncServer(object):
         while len(self._socks['in']) > 0:
 
             # use select to update all ready sockets, and check for dodgy socks
+            timeout = 20 # add timeout to keep the server appearing as working
             self._socks['read'], self._socks['write'], self._socks['bad'] = sl.select(self._socks['in'],
                                                                                     self._socks['out'],
-                                                                                    self._socks['in'])
+                                                                                    self._socks['in'],
+                                                                                    timeout)
 
-            if self._debug: self.printSockLists("Start of loop")
-            if self._debug: print self._msgBuffers
+            if not (self._socks['read'] or self._socks['write'] or self._socks['bad']):
+                lg.info("Timed out.")
+
+            # if self._debug: self.printSockLists("Start of loop")
+            # if self._debug: print self._msgBuffers
 
             # check if there are ever any socks in error
             for bSock in self._socks['bad']:
-                print "BAD SOCK:", bSock
-                if bSock == self._serverSock:
-                    raise "Bad Server Sock!"
-                # if any sock is in error or has an exception, remove entirely
+                lg.error("BAD SOCK: %s" % bSock)
                 self._socks['in'].remove(bSock)
                 if bSock in self._socks['out']:
                     self._socks['out'].remove(bSock)
+                del self._msgBuffers[bSock]
                 bSock.close()
 
 
             # loop through all readable socks
             for rSock in self._socks['read']:
-
+                lg.debug("Read loop: %s" % rSock)
+                
                 if rSock == self._serverSock:
-                    # if the serverSock is ready to read, then accept the
-                    # incoming client connection
+                    # if the serverSock is ready to read, then accept the incoming client connection
                     newClientConn, newClientAddr = rSock.accept()
                     # make non-blocking too
                     newClientConn.setblocking(0)
                     
-                    if self._debug:
-                        print "> New client connection: %s %s" % (newClientAddr, newClientConn)
+                    lg.debug("New client connection: %s %s" % (newClientAddr, newClientConn))
                     
                     # add new socket to input list, ready for potential recv'ing
                     self._socks['in'].append(newClientConn)
                     
                     # add empty buffer entry
-                    self._msgBuffers[newClientConn] = []
+                    self._msgBuffers[newClientConn] = MessageBuf()
 
                     # if not already, add new socket to the output list, ready for potential send
                     if rSock not in self._socks['out']:
@@ -93,8 +130,13 @@ class AceyncServer(object):
                     
                     if recvMsg:
                         # if message received, then add to buffers to echo later
-                        print "Message received from %s: %s" % (rSock.getpeername(), recvMsg)
-                        self._msgBuffers[rSock].append(recvMsg)
+                        lg.info("Message received from %s: %s" % (rSock.getpeername(), recvMsg))
+                        self._msgBuffers[rSock].Add(recvMsg)
+
+                        # check if message is ready to be echoed
+                        if recvMsg[-1] == "\n":
+                            lg.debug("End of line received.")
+                            self._msgBuffers[rSock].EchoReady(True)
                     else:
                         # if no message received, the client must be gone, so remove
                         self._socks['in'].remove(rSock)
@@ -103,26 +145,20 @@ class AceyncServer(object):
                         rSock.close()
 
             for wSock in self._socks['write']:
-                if self._debug: print "Write loop:", wSock
-                if self._debug: print self._msgBuffers
+                lg.debug("Write loop: %s" % wSock)
+                # if self._debug: print self._msgBuffers
 
-                # check whether there's a buffered message for this sock
-                try:
-                    echoMsg = " ".join(self._msgBuffers[wSock])
-                except KeyError:
-                    if self._debug: print "> No msg to echo back..."
-                    echoMsg = ""
-
-                if echoMsg:
+                # check whether there's a message ready to be echoed
+                if self._msgBuffers[wSock].EchoReady(): 
                     # if there is, send that bad boy back
-                    self.echoBack(wSock, echoMsg)
-                    self._msgBuffers[wSock] = []
+                    self.echoBack(wSock, self._msgBuffers[wSock].Get())
+                    self._msgBuffers[wSock].Reset() # reset message buffer, ready for another
 
-            if self._debug: self.printSockLists("End of loop")
+            # if self._debug: self.printSockLists("End of loop")
 
     def echoBack(self, sock, msg = ""):
         """echo wrapper function"""
-        print "Echoing to %s: %s" % (sock.getpeername(), msg)
+        lg.info("Echoing to %s: %s" % (sock.getpeername(), msg))
         # note: using 'sendall' lets python deal with packet splitting according
         # to network buffer
         sock.sendall(msg)
@@ -138,7 +174,23 @@ class AceyncServer(object):
         print "  BadSocks:", self._socks['bad']
         print "-"*40
 
+#---------------------------------------------------------------------#
+#---------------------------------------------------------------------#
+
 if __name__ == "__main__":
 
-    server = AceyncServer(debug = False)
+    parser = OptionParser()
+    parser.add_option("-d", "--debug",
+                  action="store_true", dest="debug", default=False,
+                  help="run server in Debug mode")
+    parser.add_option('--log', help = 'Set logging level (WARNING/DEBUG/INFO/ERROR/CRITICAL)',
+                        dest = 'loglevel', default = 'INFO')
+    (opts, args) = parser.parse_args()
+
+    if opts.debug:
+        opts.loglevel = 'DEBUG'
+
+    lg.basicConfig(level=opts.loglevel.upper())
+
+    server = AceyncServer(debug = opts.debug)
     server.startListening()
