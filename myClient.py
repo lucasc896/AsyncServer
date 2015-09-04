7c7a3b7a741041b32db3cc5ae8517157cab11c96
Chris Lucas <chris.lucas@bristol.ac.uk>
2015-09-04 11:44:21 +0100

    tidy up

--- myClient.py
+++ myClient.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import socket as sk
 from sys import argv
+from time import sleep
 
 if len(argv) > 1:
     mesg = argv[1]
@@ -9,22 +10,24 @@ else:
 
 BUFSIZE = 4096
 
-socks = [sk.socket(sk.AF_INET, sk.SOCK_STREAM),sk.socket(sk.AF_INET, sk.SOCK_STREAM)][:1]
-print socks
+# how many different sockets/clients
+socks = [sk.socket(sk.AF_INET, sk.SOCK_STREAM) for i in range(3)]
+
 serverAddress = ('localhost', 6666)
+
 for sock in socks:
-    print "> Trying to connect to server on %s (port: %s)" % serverAddress
+    # print "> Trying to connect to server on %s (port: %s)" % serverAddress
     sock.connect(serverAddress)
     print "> Connected to", serverAddress
 
-for msg in [mesg, mesg+"!!", "i'm ok.\n"]:
+for i in range(1): #how many rounds of messages
+    for msg in ["one", "two", "three\n"]:
+            
+        for sock in socks:
+            print "Sending", mesg+msg
+            sock.sendall(mesg+msg)
+            sleep(1.8)
         
     for sock in socks:
-        print "Sending", msg
-        sock.sendall(msg)
-    
-for sock in socks:
-    msg = sock.recv(BUFSIZE)
-    print "Received: %s" % msg
-    if not msg:
-        sock.close()
+        msg = sock.recv(BUFSIZE)
+        print "Received: %s" % msg
